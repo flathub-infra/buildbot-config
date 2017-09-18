0b22ac58e586cda7c24a20ede614ac04e664d83b
Alexander Larsson <alexl@redhat.com>
2017-09-18 14:09:11 +0200

    Forgot to add import script

---
+++ scripts/import.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+REPO=$1
+REMOTE=$2
+IMPORT_REFS=$3
+GPG_HOMEDIR=$4
+GPG_KEY=$5
+
+#set -x
+
+declare -A MIRROR_REFS
+
+REFS=$(ostree --repo=$REPO remote refs  $REMOTE | sed s/$REMOTE://)
+for REF in $REFS; do
+    [[ ${REF} != 'runtime/'* && ${REF} != 'app/'* ]] && continue
+    IFS='/' read -ra PARTS <<< "$REF"
+    for IMPORT_REF in $IMPORT_REFS; do
+        IFS='/' read -ra IMPORT_PARTS <<< "$IMPORT_REF"
+        [[ ${PARTS[1]} != ${IMPORT_PARTS[0]}* ]] && continue
+        [[ ${PARTS[2]} != ${IMPORT_PARTS[1]}*  && ${IMPORT_PARTS[1]} != "" ]] && continue
+        [[ ${PARTS[3]} != ${IMPORT_PARTS[2]}*  && ${IMPORT_PARTS[2]} != "" ]] && continue
+        #echo Matched: $REF $IMPORT_REF
+        COMMIT=$(ostree --repo=$REPO show $REF 2> /dev/null| grep commit)
+        #echo COMMIT=$COMMIT
+        MIRROR_REFS[$REF]=$COMMIT
+        break
+    done
+done
+
+echo pulling ${!MIRROR_REFS[@]}
+ostree --repo=$REPO pull --mirror $REMOTE ${!MIRROR_REFS[@]}
+
+for REF in ${!MIRROR_REFS[@]}; do
+    OLD_COMMIT=${MIRROR_REFS[$REF]}
+    NEW_COMMIT=$(ostree --repo=$REPO show $REF 2> /dev/null| grep commit)
+    echo $REF: $OLD_COMMIT is now $NEW_COMMIT
+    if [[ $OLD_COMMIT != $NEW_COMMIT ]]; then
+        echo signing $REF
+        ostree gpg-sign --repo=$REPO --gpg-homedir=$GPG_HOMEDIR $REF $GPG_KEY
+    fi
+done
+
+flatpak build-update-repo --gpg-homedir=$GPG_HOMEDIR --gpg-sign=$GPG_KEY $REPO
