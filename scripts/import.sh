83e51ccb702c61e78bc208c31ffce9846ee4b0f5
Alexander Larsson <alexl@redhat.com>
2017-10-04 18:43:10 +0200

    Don't fail if no local commit

--- scripts/import.sh
+++ scripts/import.sh
@@ -21,7 +21,7 @@ for REF in $REFS; do
         [[ ${PARTS[2]} != ${IMPORT_PARTS[1]}*  && ${IMPORT_PARTS[1]} != "" ]] && continue
         [[ ${PARTS[3]} != ${IMPORT_PARTS[2]}*  && ${IMPORT_PARTS[2]} != "" ]] && continue
         #echo Matched: $REF $IMPORT_REF
-        COMMIT=$(ostree --repo=$REPO show $REF 2> /dev/null| grep commit)
+        COMMIT=$(ostree --repo=$REPO show $REF 2> /dev/null| grep commit || true)
         #echo COMMIT=$COMMIT
         MIRROR_REFS[$REF]=$COMMIT
         break
