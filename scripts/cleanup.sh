c0e916fe36274f98c9b860f834741e74269c288f
Robert McQueen <rob@endlessm.com>
2018-01-29 15:05:45 +0000

    cleanup: print dates of found folders

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -9,6 +9,7 @@ for i in ../../build-*; do
     if test -d $i; then
          pushd $i
          echo Scanning `basename $i`
+         find . -maxdepth 1 -type d -printf '%t %p\n'
          du -csh *
          popd
     fi
