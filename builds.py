ec851509028a45f9f6f9fe66f8dbffc91eb010c7
Alexander Larsson <alexl@redhat.com>
2018-03-22 10:08:43 +0100

    builds: Drop global module data
    All data are now in objects created by the code, owned
    by master.cfg. This means reloading the master config re-loads
    the builds.json file.

--- builds.py
+++ builds.py
@@ -6,18 +6,19 @@ import subprocess, re, json
 import os.path
 import utils
 
-repos = {}
-builds = {}
-imports = {}
-default_repo = None
-by_base_uri = {}
-reverse_deps = {}
+def id_is_valid(possible_id):
+    return len(possible_id.split('.')) >= 3
 
-def reverse_dependency_lookup(buildname):
-    res = []
-    for id in reverse_deps.get(buildname, []):
-        res.append(lookup_by_name(id))
-    return res
+def build_name_to_id(buildname):
+    return buildname.split("/", 1)[0]
+
+def strip_dot_git(module):
+    if module.endswith(".git"):
+        module = module[:-4]
+    return module
+
+def git_modules_equal(module_a, module_b):
+    return strip_dot_git (module_a) == strip_dot_git (module_b)
 
 class BuildDataRepo:
     def __init__(self, name, json):
@@ -35,11 +36,6 @@ class BuildDataInfo:
         self.only_arches = json.get("only-arches", None)
         self.custom_buildcmd = json.get("custom-buildcmd", False)
         self.base = json.get("base", None)
-
-        if self.base:
-            l = reverse_deps.get(self.base, [])
-            l.append(buildname)
-            reverse_deps[self.base] = l
 
     def get_git_branch(self):
         if self.git_branch:
@@ -69,157 +65,158 @@ class BuildData:
     def __str__(self):
         return "%s %s %s %s %s" % (self.id, self.fp_branch, self.url, self.git_branch, "official" if self.official else "test")
 
-def load_config(filename):
-    global repos, builds, imports, default_repo, by_base_uri
-    f = open(filename, 'r')
-    config = utils.json_to_ascii(json.loads(f.read ()))
-    for k in config["repos"]:
-        repos[k] = BuildDataRepo(k, config["repos"][k])
-    for k in config["builds"]:
-        builds[k] = BuildDataInfo(k, config["builds"][k], repos)
-    default_repo = repos["default"]
-    for n in repos:
-        r = repos[n]
-        by_base_uri[r.base] = r
-    imports = config["imports"]
+class Builds:
+    def __init__(self, filename):
+        self.repos = {}
+        self.builds = {}
+        self.imports = {}
+        self.default_repo = None
+        self.by_base_uri = {}
+        self.reverse_deps = {}
 
-def lookup_by_name(buildname):
-    split = buildname.split("/", 1)
-    id = split[0]
-    fp_branch = None
-    if len(split) > 1:
-        fp_branch = split[1]
+        f = open(filename, 'r')
+        config = utils.json_to_ascii(json.loads(f.read ()))
+        for k in config["repos"]:
+            self.repos[k] = BuildDataRepo(k, config["repos"][k])
+        for k in config["builds"]:
+            i = BuildDataInfo(k, config["builds"][k], self.repos)
+            self.builds[k] = i
+            if i.base:
+                l = self.reverse_deps.get(i.base, [])
+                l.append(i.buildname)
+                self.reverse_deps[i.base] = l
 
-    custom_buildcmd = False
-    only_arches = None
+        self.default_repo = self.repos["default"]
+        for n in self.repos:
+            r = self.repos[n]
+            self.by_base_uri[r.base] = r
+        self.imports = config["imports"]
 
-    if builds.has_key(buildname):
-        info = builds[buildname]
-        repo = info.repo
-        module = info.get_git_module()
-        git_branch = info.get_git_branch()
-        custom_buildcmd = info.custom_buildcmd
-        only_arches = info.only_arches
-    else: # No build, create default
-        if fp_branch != None or id_used_in_buildname(id):
-           raise Exception("No defined build %s" % buildname)
-        repo = default_repo
-        module = "%s.git" % id
-        git_branch = repo.default_branch
+    def reverse_dependency_lookup(self, buildname):
+        res = []
+        for id in self.reverse_deps.get(buildname, []):
+            res.append(self.lookup_by_name(id))
+            return res
+
+    def lookup_by_name(self, buildname):
+        split = buildname.split("/", 1)
+        id = split[0]
+        fp_branch = None
+        if len(split) > 1:
+            fp_branch = split[1]
+
+        custom_buildcmd = False
+        only_arches = None
+
+        if self.builds.has_key(buildname):
+            info = self.builds[buildname]
+            repo = info.repo
+            module = info.get_git_module()
+            git_branch = info.get_git_branch()
+            custom_buildcmd = info.custom_buildcmd
+            only_arches = info.only_arches
+        else: # No build, create default
+            if fp_branch != None or self.id_used_in_buildname(id):
+                raise Exception("No defined build %s" % buildname)
+            repo = self.default_repo
+            module = "%s.git" % id
+            git_branch = repo.default_branch
+            if not id_is_valid(id):
+                raise Exception("Invalid build name %s" % buildname)
+
+        d = BuildData(id, fp_branch)
+        d.url = "%s/%s" % (repo.base, module)
+        d.git_branch = git_branch
+        d.official = True
+        d.custom_buildcmd = custom_buildcmd
+        d.only_arches = only_arches
+        return d
+
+    def find_build_by_repo_module_branch(self, repo, git_module, git_branch):
+        for b_name in self.builds:
+            info = self.builds[b_name]
+            # Match repo (base uri)
+            if repo.name != info.reponame:
+                continue
+            # Match module name
+            if not git_modules_equal (git_module, info.get_git_module()):
+                continue
+            # Match git branch
+            if git_branch != info.get_git_branch():
+                continue
+            return b_name
+        return None
+
+    def id_used_in_buildname(self, id):
+        for b_name in self.builds:
+            if id == build_name_to_id(b_name):
+                return True
+        return False
+
+    # The id here is optional, its never passed when
+    # we get git change notifications, but it may
+    # be optionally set by the user for test builds
+    # because in that case the app id may differ
+    # from the repo name (git module)
+    def lookup_by_git(self, git_url, git_branch, optional_id=None):
+        if optional_id and '/' in optional_id:
+            raise Exception("Invalid id %s, slash not allowed" % optional_id)
+
+        s = git_url.rsplit('/', 1);
+        base = s[0]
+        git_module = s[1]
+
+        if optional_id:
+            id = optional_id
+        else:
+            id = strip_dot_git(git_module)
+
+        repo = self.by_base_uri.get(base, None)
+        if repo:
+            # Maybe it exactly matches a specified build name (and id if specified)
+            exact_match = self.find_build_by_repo_module_branch(repo, git_module, git_branch)
+            if exact_match and (not optional_id or optional_id == build_name_to_id(exact_match)):
+                return self.lookup_by_name(exact_match)
+
+            # It could be a default build name from the git module if:
+            # * It matches the default repo
+            # * It matches the default branch in the default repo
+            # * The git module name matches the id
+            # * And there is no explicit buildnames with that id as prefix (so, e.g no
+            #    default "org.kde.Sdk" if a "org.kde.Sdk/5.9" is configured, we only allow
+            #   exact matches in that case)
+            default_git_branch = self.default_repo.default_branch
+            if (id_is_valid(id) and
+                repo == self.default_repo and
+                git_branch == default_git_branch and
+                git_modules_equal(id, git_module) and
+                not self.id_used_in_buildname(id)):
+                return self.lookup_by_name(id)
+
+        # Make sure the module is a valid name
         if not id_is_valid(id):
-            raise Exception("Invalid build name %s" % buildname)
+            raise Exception("Invalid id %s" % id)
 
-    d = BuildData(id, fp_branch)
-    d.url = "%s/%s" % (repo.base, module)
-    d.git_branch = git_branch
-    d.official = True
-    d.custom_buildcmd = custom_buildcmd
-    d.only_arches = only_arches
-    return d
+        # Make up a "fake" BuildData for test builds
+        fake = BuildData(id, None)
+        fake.url = git_url
+        fake.git_branch = git_branch
 
-def id_is_valid(possible_id):
-    return len(possible_id.split('.')) >= 3
+        return fake
 
-def build_name_to_id(buildname):
-    return buildname.split("/", 1)[0]
-
-def strip_dot_git(module):
-    if module.endswith(".git"):
-        module = module[:-4]
-    return module
-
-def git_modules_equal(module_a, module_b):
-    return strip_dot_git (module_a) == strip_dot_git (module_b)
-
-def find_build_by_repo_module_branch(repo, git_module, git_branch):
-    for b_name in builds:
-        info = builds[b_name]
-        # Match repo (base uri)
-        if repo.name != info.reponame:
-            continue
-        # Match module name
-        if not git_modules_equal (git_module, info.get_git_module()):
-            continue
-        # Match git branch
-        if git_branch != info.get_git_branch():
-            continue
-        return b_name
-    return None
-
-def id_used_in_buildname(id):
-    for b_name in builds:
-        if id == build_name_to_id(b_name):
-            return True
-    return False
-
-# The id here is optional, its never passed when
-# we get git change notifications, but it may
-# be optionally set by the user for test builds
-# because in that case the app id may differ
-# from the repo name (git module)
-def lookup_by_git(git_url, git_branch, optional_id=None):
-    if optional_id and '/' in optional_id:
-        raise Exception("Invalid id %s, slash not allowed" % optional_id)
-
-    s = git_url.rsplit('/', 1);
-    base = s[0]
-    git_module = s[1]
-
-    if optional_id:
-        id = optional_id
-    else:
-        id = strip_dot_git(git_module)
-
-    repo = by_base_uri.get(base, None)
-    if repo:
-        # Maybe it exactly matches a specified build name (and id if specified)
-        exact_match = find_build_by_repo_module_branch(repo, git_module, git_branch)
-        if exact_match and (not optional_id or optional_id == build_name_to_id(exact_match)):
-            return lookup_by_name(exact_match)
-
-        # It could be a default build name from the git module if:
-        # * It matches the default repo
-        # * It matches the default branch in the default repo
-        # * The git module name matches the id
-        # * And there is no explicit buildnames with that id as prefix (so, e.g no
-        #    default "org.kde.Sdk" if a "org.kde.Sdk/5.9" is configured, we only allow
-        #   exact matches in that case)
-        default_git_branch = default_repo.default_branch
-        if (id_is_valid(id) and
-            repo == default_repo and
-            git_branch == default_git_branch and
-            git_modules_equal(id, git_module) and
-            not id_used_in_buildname(id)):
-            return lookup_by_name(id)
-
-    # Make sure the module is a valid name
-    if not id_is_valid(id):
-        raise Exception("Invalid id %s" % id)
-
-    # Make up a "fake" BuildData for test builds
-    fake = BuildData(id, None)
-    fake.url = git_url
-    fake.git_branch = git_branch
-
-    return fake
-
-
-config_file = 'builds.json'
-if __name__ == "__main__":
-    config_file = 'builds-test.json'
-
-load_config(config_file)
-
+# Tests:
 
 def verify_by_name_exception(buildname):
+    global test_builds
     print("testing lookup_by_name(%s)" % buildname)
     try:
-        r = lookup_by_name(buildname)
+        r = test_builds.lookup_by_name(buildname)
     except Exception as e:
         return
     raise Exception("No exception for buildname %s when one was expected" % buildname)
 
 def verify_by_name(buildname, id = None, url = None, git_branch = None, fp_branch = None):
+    global test_builds
     if id == None:
         id = build_name_to_id(buildname)
     if url == None:
@@ -229,7 +226,7 @@ def verify_by_name(buildname, id = None, url = None, git_branch = None, fp_branc
 
     print("testing lookup_by_name(%s)" % buildname)
     try:
-        res = lookup_by_name(buildname)
+        res = test_builds.lookup_by_name(buildname)
     except Exception as e:
         raise Exception("Got unexpected exception %s buildname %s" % (e, buildname))
     if not res.official:
@@ -271,20 +268,22 @@ def test_lookup_by_name():
     verify_by_name("org.kde.Sdk/5.11", fp_branch="5.11", url="https://github.com/KDE2/flatpak-kde-runtime.git", git_branch="kde-5-11")
 
 def verify_by_git_exception(url, git_branch, optional_id = None):
+    global test_builds
     print("testing lookup_by_git(%s, %s, %s)" % (url, git_branch, optional_id))
     try:
-        r = lookup_by_git(url, git_branch, optional_id)
+        r = test_builds.lookup_by_git(url, git_branch, optional_id)
     except Exception as e:
         return
     raise Exception("No exception for git lookup when one was expected")
 
 def verify_by_git(url_arg, git_branch, id, optional_id = None,
                   official = False, url = None, fp_branch = None):
+    global test_builds
     if url == None:
         url = url_arg
     print("testing lookup_by_git(%s, %s, %s)" % (url_arg, git_branch, optional_id))
     try:
-        res = lookup_by_git(url_arg, git_branch, optional_id)
+        res = test_builds.lookup_by_git(url_arg, git_branch, optional_id)
     except Exception as e:
         raise Exception("Got unexpected exception '%s' when looking up by git" % e)
     if url != res.url:
@@ -387,6 +386,9 @@ def test_lookup_by_git():
                            url="https://github.com/KDE2/flatpak-kde-runtime.git")
     verify_by_git_test("https://github.com/KDE2/org.kde.Sdk.git", "kde-branch", "org.kde.Sdk")
 
+test_builds = None
 if __name__ == "__main__":
+    test_builds = Builds('builds-test.json')
+
     test_lookup_by_name()
     test_lookup_by_git()
