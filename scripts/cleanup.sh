c99439ade9ed747226e9b652af9d89a2454974ff
Alexander Larsson <alexl@redhat.com>
2018-04-01 11:51:38 +0200

    Fixup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -12,7 +12,7 @@ for i in ../../build-*; do
          pushd $i
          echo Scanning `basename $i`
          du -chs *
-         for i */.flatpak-builder/rofiles/rofiles-*; do
+         for i in */.flatpak-builder/rofiles/rofiles-*; do
              fusermount -u -z $i || true
          done
          find -mindepth 1 -maxdepth 1 -type d -mtime +2 -print0 | xargs -0t rm -rf
