5d8c43082120558409af9d17cdb0ac062910a8e1
Alexander Larsson <alexl@redhat.com>
2018-04-01 11:49:19 +0200

    fixup cleanup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+shopt -s nullglob
 
 echo Free space on build disk:
 df -h .
@@ -10,9 +11,10 @@ for i in ../../build-*; do
     if test -d $i; then
          pushd $i
          echo Scanning `basename $i`
-         ls -lat
-         du -c --max-depth=1 | sort -n
-         umount -v */.flatpak-builder/rofiles/rofiles-* || true
+         du -chs *
+         for i */.flatpak-builder/rofiles/rofiles-*; do
+             fusermount -u -z $i || true
+         done
          find -mindepth 1 -maxdepth 1 -type d -mtime +2 -print0 | xargs -0t rm -rf
          popd
     fi
